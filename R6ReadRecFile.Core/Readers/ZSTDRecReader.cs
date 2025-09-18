1e81e951cc885d7c56f53a3ce33080a32e64e2ef
SacBruine <gregorytacquet@gmail.com>
2025-09-18 19:59:48 +0200

    #4 : Start of creation of ZSTD compression reading

---
+++ R6ReadRecFile.Core/Readers/ZSTDRecReader.cs
@@ -0,0 +1,23 @@
+﻿using R6ReadRecFile.Core.Utils;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using ZstdSharp;
+
+namespace R6ReadRecFile.Core.Readers
+{
+    public class ZSTDRecReader
+    {
+        string filePath;
+        public ZSTDRecReader(string filePath)
+        {
+            this.filePath = filePath;
+        }
+        public List<string> GetData()
+        {
+            byte[] data = File.ReadAllBytes(filePath);
+            var decompressData = ZSTDHelper.DecompressFile(data);
+            return decompressData;
+
+        }
+    }
+}
---
+++ R6ReadRecFile.Core/Services/ZSTDParser.cs
@@ -0,0 +1,21 @@
+﻿using R6ReadRecFile.Core.Interfaces;
+using R6ReadRecFile.Core.Models;
+using R6ReadRecFile.Core.Readers;
+
+namespace R6ReadRecFile.Core.Services
+{
+    public class ZSTDParser : IRecParser
+    {
+        public RecFile Parse(string pathFile)
+        {
+            ZSTDRecReader zSTDParser = new ZSTDRecReader(pathFile);
+            zSTDParser.GetData();
+            return null;
+        }
+
+        public RecFile Parse(FileStream file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
